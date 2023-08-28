c8691d533025fd4bead0441c3a909a6048780769
Темирлан Карипжанов <@itstep.loc>
2023-08-28 20:48:16 +0600

    button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
+import {ref} from 'vue'
+const counter=ref(0);
+
+const  increase=()=>{
+    counter.value++//Обращение именно к value, подменяем значение на то что
+}
+const  decrease=()=>{
+    counter.value--
+}
+
 </script>
 
 <template>
- <div> 
- Работает!
+ <div class="test"> 
+    {{counter}}
+    <button @click="increase"> Увеличить </button>
+    <button @click="decrease"> Уменьшить </button>
  </div>
 </template>
 
