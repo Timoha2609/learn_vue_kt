d9c1c058d72229cd8fc076dc69528fce91c7d4a0
Темирлан Карипжанов <@itstep.loc>
2023-09-04 18:42:52 +0600

    addswitch

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,9 +2,10 @@
 import Sidebar from 'primevue/sidebar'
 import InputText from 'primevue/inputtext';
 import Textarea from 'primevue/textarea';
+import InputSwitch from 'primevue/inputswitch';
 import Button from 'primevue/button';
 import { ref } from "vue";
-
+const checked = ref(false);
 const visible = ref(false);
 
 
@@ -14,8 +15,11 @@ const visible = ref(false);
     <div class="card flex justify-content-center">
         <Sidebar v-model:visible="visible">
             <h2>Sidebar</h2>
+            <label>Inputext</label>
             <InputText type="text" v-model="value" />
+            <label>Textarea</label>
             <Textarea v-model="value" rows="5" cols="30" />    
+            <InputSwitch v-model="checked" />
         </Sidebar>
         <Button icon="pi pi-arrow-right" @click="visible = true" />
     </div>   
