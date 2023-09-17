6ad9b18623b62485070bc2b792571af64c0d9636
Айгерим <gera.kuatova@gmail.com>
2023-09-17 11:42:24 +0600

    dobavilkarsotuvfooter

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,25 +1,45 @@
 <script setup>
-
+import Fieldset from 'primevue/fieldset';
 
 </script>
 
 <template> 
-  <footer class="footer">
-      <p>MAde by Karipzhanov.T</p>
-  </footer>
+
+    <div class="card footer"  >
+      <Fieldset legend="Creator Name" :toggleable="true">
+          <p>
+              Made by Karipzhanov.T
+          </p>
+      </Fieldset>
+  </div>
+ 
 </template>
 
 <style scoped> 
-.footer {
-  background-color: #00bcd4; 
-  color: white; 
+.footer,.fieldset{
+  background-color: #00bcd4;
+  color: white;
   padding: 20px 0;
   text-align: center;
-  position: absolute;
+  position: relative;
   bottom: 0;
   width: 100%;
 }
+:deep(.p-toggleable-content){
+  background-color: #00bcd4 !important;
+}
+:deep(.p-fieldset-legend){
+  background-color: #FFF59D !important;
+}
 
+:deep(.p-fieldset-legend-text){
+  color:#EF9A9A !important;
+}
+
+:deep(.p-fieldset-toggleable){
+  background-color: #90CAF9 !important;
+  color:#C5E1A5 !important;
+}
 
 
 </style>
